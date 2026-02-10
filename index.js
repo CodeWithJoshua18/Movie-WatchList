33a215684c3b161c547b9f24dfdb907123d26ed9
CodeWithJoshua18 <generaljosh5311@gmail.com>
2026-02-10 18:26:22 +0300

    refactor: clean up movie constructor and improve form handling

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-// creating movie constructor
-function Movie(title, director, duration, isWatched){
-    this.title = title;
-    this.director = director;
-    this.duration = duration;
-    this.isWatched = isWatched;
+function Movie(title, director, duration, isWatched) {
+  this.title = title;
+  this.director = director;
+  this.duration = duration;
+  this.isWatched = isWatched;
+}
+
+Movie.prototype.info = function () {
+  const readStatus = this.isWatched ? "Has been watched" : "Watch now";
+  return `${this.title} by ${this.director} is ${this.duration} hours long. ${readStatus}`;
 };
 
-Movie.prototype.info = function(){
-    const readStatus = this.isWatched ? 'Has been watched' : 'Watch Now';
-    return`${this.title} by ${this.director} is ${this.duration} hours long. ${this.isWatched}`
-};
-
-// creating watchlist array to store added movies
 const myWatchList = [];
 
-const form = document.getElementById('#movieForm');
-const movieDiv = document.getElementById('movieList');
+const form = document.getElementById("movieForm");
+const movieList = document.getElementById("movieList");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const director = document.getElementById("director").value;
+  const duration = parseInt(document.getElementById("duration").value);
+  const isWatched = document.getElementById("isWatched").checked;
+
+  const newMovie = new Movie(title, director, duration, isWatched);
+  myWatchList.push(newMovie);
+
+  displayMovie();
+  form.reset();
 });
 
-// get values from form
-const title = document.getElementById('#title').value;
-const director = document.getElementById('#director').value;
-const duration = parseInt(document.getElementById('#duaration').value);
-const isWatched = document.getElementById('#isWatched').value;
+function displayMovie() {
+  movieList.innerHTML = "";
 
-// create new movie
-const newMovie = new Movie(title, director, duration, isWatched);
-
-// add it to the watchlist array
-myWatchList.push(newMovie);
-
-// display the movies
+  myWatchList.forEach((movie) => {
+    const div = document.createElement("div");
+    div.textContent = movie.info();
+    movieList.appendChild(div);
+  });
+}
